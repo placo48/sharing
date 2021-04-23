1fabebca4b3c95acf868bf48b2c546e56c9b05f1
szczor <52785452+szczor@users.noreply.github.com>
2021-04-23 15:45:13 +0200

    Add files via upload

--- test_uzytki.py
+++ test_uzytki.py
@@ -1,9 +1,22 @@
 from uzytki import define_starting_df
 import pandas as pd
+from uzytki import new_clients
 
 
 def test_define_starting_df():
-    assert define_starting_df(3, [10, 20, 50, 100]).equals(pd.DataFrame({'Q0': [10], 'Q1': [20], 'Q2': [50], 'Q3': [100]},
-                                                                    columns=['Q0', 'Q1', 'Q2', 'Q3']))
+    assert define_starting_df(3, [10, 20, 50, 100]).equals(
+        pd.DataFrame({'Q0': [10], 'Q1': [20], 'Q2': [50], 'Q3': [100]},
+                     columns=['Q0', 'Q1', 'Q2', 'Q3']))
     assert define_starting_df(2, [10, 20, 50]).equals(pd.DataFrame({'Q0': [10], 'Q1': [20], 'Q2': [50]},
-                                                                    columns=['Q0', 'Q1', 'Q2']))
+                                                                   columns=['Q0', 'Q1', 'Q2']))
+
+
+def test_new_clients():
+    Q = [10, 20, 50]
+    arg1 = 10
+    arg2 = 2
+    x = new_clients(Q, arg1, arg2)
+    print(x)
+
+if __name__=='__main__':
+    test_new_clients()
--- uzytki.py
+++ uzytki.py
@@ -1,20 +1,32 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sympy import solve, Eq, symbols
 
 
 # starting parameters
-def define_parameters(n_=3, s0=0.1, s1=0.6, s2=0.65, p0=0.5,
-                      p1=0.3, p2=0.3, p3=0.9, r0=0.4, r1=0.1, r2=0.05, r3=0.1,
-                      Q00=10, Q01=20, Q02=50, Q03=100, k_=30, t_=0):
+def define_parameters(n_=3, s0=0.1, s1=0.4, s2=0.65, q0=0.2, q1=0.2, q2=0, q3=0,
+                      p0=0.5, p1=0.3, p2=0.3, p3=0.9, r0=0.2, r1=0.1, r2=0.05, r3=0.1,
+                      Q00=10, Q01=20, Q02=50, Q03=100, t_=0):
     n = n_  # number of starting generations
     s = [s0, s1, s2]
+    q = [q0, q1, q2, q3]
     p = [p0, p1, p2, p3]
     r = [r0, r1, r2, r3]
     Q = [Q00, Q01, Q02, Q03]
-    k = k_
     t = t_
-    return n, s, p, r, Q, k, t
+    return n, s, q, p, r, Q, t
+
+
+def new_clients(Q, *args):
+    srednia = sum(Q) / len(Q)
+    srednia = srednia / args[0]
+    variance = args[1]
+    x, y = symbols('x y')
+    eq1 = Eq(srednia - x * y, 0)
+    eq2 = Eq(variance - x * y * y, 0)
+    sol = solve((eq1, eq2), (x, y))[0]
+    return np.random.gamma(sol[0], sol[1])
 
 
 def define_starting_df(n, Q):
@@ -25,28 +37,39 @@ def define_starting_df(n, Q):
     return df
 
 
-def next_year(df, df2, n, t, s, p, k):
+def next_year(df, df2, n, t, s, p, k, k_):
     row = [0] * (n + 1)
     t = t + 1
     for j in range(n + 1):
-        if j != n:
+        if j != n and j != n - 1:
             row[j] = round(s[j] * df.iloc[t - 1][-(n - j + 1)])
+        elif j == n - 1:
+            row[j] = round(s[j] * df.iloc[t - 1][-(n - j + 1)])
+            for i in range(n + 1):
+                row[j] += round(q[i] * df.iloc[t - 1][-n + i - 1])
+            row[j] += round(k_)
         elif j == n:
             for i in range(n + 1):
                 row[j] += round(p[i] * df.iloc[t - 1][-n + i - 1])
-            row[j] += k
+            row[j] += round(k)
     size = len(df.columns)
     no_zeros = size - (n + 1)
     list_zeros = [0] * no_zeros
     row = list_zeros + row
     df.loc[t] = row
 
-    new_users = 0
+    new_users_1 = 0
     for l in range(n):
-        new_users += round(p[l] * df.iloc[t - 1][-n + l - 1])
-    new_users += k
+        new_users_1 += round(p[l] * df.iloc[t - 1][-n + l - 1])
+    new_users_1 += round(k)
+    new_users_2 = 0
+    for l in range(n):
+        new_users_2 += round(q[l] * df.iloc[t - 1][-n + l - 1])
+    new_users_2 += round(k_)
     last_row = df2.loc[t - 1]
-    last_row[-1] += new_users
+    last_row[-1] += new_users_1
+    if len(last_row) > 1:
+        last_row[-2] += new_users_2
     df2.loc[t] = last_row
     return df, df2, t
 
@@ -72,14 +95,18 @@ def plot_df(df):
 
 
 if __name__ == '__main__':
-    n, s, p, r, Q, k, t = define_parameters()
-    df = define_starting_df(n, Q)
+    n, s, q, p, r, Q, t = define_parameters()
+    df1 = define_starting_df(n, Q)
     df2 = create_number_of_users_df(n, Q)
-    df, df2, t = next_year(df, df2, n, t, s, p, k)
-    for i in range(6):
-        df, df2 = new_generation(df, df2)
-        df, df2, t = next_year(df, df2, n, t, s, p, k)
-        df, df2, t = next_year(df, df2, n, t, s, p, k)
-    print(df)
+    k = new_clients(Q, 5, 5)
+    k_ = new_clients(Q, 15, 5)
+    df1, df2, t = next_year(df1, df2, n, t, s, p, k, k_)
+    for i in range(4):
+        df1, df2 = new_generation(df1, df2)
+        k = new_clients(Q, 5, 5)
+        df1, df2, t = next_year(df1, df2, n, t, s, p, k, k_)
+        k = new_clients(Q, 5, 5)
+        df1, df2, t = next_year(df1, df2, n, t, s, p, k, k_)
+    print(df1)
     print(df2)
     plot_df(df2)
